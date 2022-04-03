67dc527574a6629a878e0e0c7fe8eae37d26f87e
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-04 01:01:37 +0600

    Header Added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+const Header = () => {
+    return (
+        <header className='flex justify-center md:justify-between items-center sticky px-20 py-8 text-2xl font-bold font-mono bg-[#161E35]'>
+        <div className='hidden md:block text-3xl'>
+            <Link to='./' >BOOKISH</Link>
+        </div>
+        <nav className='flex gap-4 text-xl font-mono'>
+            <CustomLink to='/'>Home</CustomLink>
+            <CustomLink to='/review'>REVIEW</CustomLink>
+            <CustomLink to='/dashboard'>DASHBOARD</CustomLink>
+            <CustomLink to='/contact'>CONTACT</CustomLink>
+            <CustomLink to='/about'>ABOUT</CustomLink>
+        </nav>
+    </header>
+    );
+};
+
+export default Header;
