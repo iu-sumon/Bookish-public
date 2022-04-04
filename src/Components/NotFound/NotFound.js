413d6c2731ad2fb630e004d1da3721c1b84591aa
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-04 21:20:41 +0600

    correction added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
     return (
         <div>
-            <div className='font-mono'>
+            <div>
                 <h1 className='text-[200px] font-extrabold text-cyan-500'>404</h1>
                 <p> OPPS! PAGE NOT FOUND </p>
                 <p>Sorry, the page you're looking for doesn't exist.</p>
             </div>
              <div className='mt-5'>
-              <Link className='bg-orange-500 px-5 py-3 mt-5 text-white rounded-2xl shadow-lg' to='/' >RETURN HOME</Link>
+             <Link to='/books' type="button" className="py-3 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-400 hover:bg-gray-200 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-400 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Return To Home</Link>
              </div>
         </div>
     );
