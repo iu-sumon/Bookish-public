d98a9d51d47e1a496b64c9c07ced2711b5c87b9e
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-04 21:24:11 +0600

    correction added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -10,7 +10,7 @@ const NotFound = () => {
                 <p>Sorry, the page you're looking for doesn't exist.</p>
             </div>
              <div className='mt-5'>
-             <Link to='/books' type="button" className="py-3 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-400 hover:bg-gray-200 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-400 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Return To Home</Link>
+             <Link to='/' type="button" className="py-3 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-400 hover:bg-gray-200 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-400 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Return To Home</Link>
              </div>
         </div>
     );
