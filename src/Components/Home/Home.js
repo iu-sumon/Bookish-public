1b29f23beb7a7ce3cedac74d1feafaeedf79b390
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-04 08:14:13 +0600

    review component added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 import photo1 from '../../images/1.jpg'
 const Home = () => {
     return (
-        <div className='grid grid-cols-2 justify-items-center items-center px-5 py-5 bg-[#5D97FF]'>
+        <div className='grid grid-cols-2 justify-items-center px-4 py-5 bg-[#5D97FF] width-full'>
             <div>
-               <h1>Books are a uniquely portable magic</h1>
-               <p>Life is a struggle for single father Chris Gardner. Evicted from their apartment, he and his young son find themselves alone with no place to go. Even though Chris eventually lands a job as an intern at a prestigious brokerage firm, the position pays no money. The pair must live in shelters and endure many hardships, but Chris refuses to give in to despair as he struggles to create a better life for himself and his son</p>
+               <h1 className='text-[45px] text-white  mb-10'>Books are a uniquely portable magic</h1>
+               <p className='text-white'>Life is a struggle for single father Chris Gardner. Evicted from their apartment, he and his young son find themselves alone with no place to go. Even though Chris eventually lands a job as an intern at a prestigious brokerage firm, the position pays no money. The pair must live in shelters and endure many hardships, but Chris refuses to give in to despair as he struggles to create a better life for himself and his son</p>
             </div>
-            <div>
-                <img src={photo1} alt="" />
+            <div className='border-2 px-1 py-1 rounded-lg'>
+                <img className='rounded-lg' src={photo1} alt="" />
             </div>
         </div>
     );
--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import useReview from '../../hooks/useReview';
+import ReviewCart from '../ReviewCart/ReviewCart';
 
 const Review = () => {
+    const [reviews]=useReview()
     return (
-        <div>
+        <div className='grid md:grid-cols-3 justify-center items-center w-[70%] mx-auto gap-5 my-10 border-4 px-3 py-3'>
             
+            {
+                reviews.map(review=><ReviewCart
+                review={review}
+                key={review.id}
+                ></ReviewCart>)
+            }
         </div>
     );
 };
