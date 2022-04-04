7e49016fc19b97e8b0009798d6dea027efe8b692
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-04 12:39:18 +0600

    DashBoard added

--- src/Components/DashBoard/DashBoard.js
+++ src/Components/DashBoard/DashBoard.js
@@ -1,9 +1,42 @@
 import React from 'react';
-
+import useData from '../../hooks/useData';
+import { Line, LineChart,Area, AreaChart,Bar,BarChart, Tooltip, XAxis, YAxis ,Legend} from 'recharts';
 const DashBoard = () => {
+    const [data] = useData();
     return (
-        <div>
-            
+        <div className='grid grid-cols-2 justify-center items-center gap-5 w-[80%] mx-auto py-5'>
+            <div className='shadow-lg border-2 rounded-2xl p-3'>
+                 <h1>Month VS Sell</h1>
+                <LineChart width={400} height={300} data={data}>
+                    <Line  dataKey='month'></Line>
+                    <Line dataKey='sell'></Line>
+                    <Legend/>
+                    <Tooltip></Tooltip>
+                    <XAxis dataKey="month" />
+                    <YAxis dataKey="sell" />
+                </LineChart>
+            </div>
+            <div className='shadow-lg border-2 rounded-2xl p-3'>
+                <h1>Investment VS Revenue</h1>
+                <AreaChart width={400} height={300} data={data}>
+                    <Area dataKey='investment'></Area>
+                    <Area dataKey='revenue'></Area>
+                    <Legend/>
+                    <Tooltip></Tooltip>
+                    <XAxis dataKey="month" />
+                    <YAxis dataKey="investment" />
+                </AreaChart>
+            </div>
+             <div className='shadow-lg border-2 rounded-2xl p-3'>
+                 <BarChart width={400} height={300} data={data}>
+                    <Bar  dataKey='investment' fill="#8884d8"></Bar>
+                    <Bar dataKey='revenue' fill="#82ca9d"></Bar>
+                    <Legend/>
+                    <Tooltip></Tooltip>
+                    <XAxis dataKey="month" />
+                    <YAxis dataKey="investment" />
+                 </BarChart>
+             </div>
         </div>
     );
 };
--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -3,9 +3,9 @@ import useReview from '../../hooks/useReview';
 import ReviewCart from '../ReviewCart/ReviewCart';
 
 const Review = () => {
-    const [reviews]=useReview()
+    const [reviews]=useReview();
     return (
-        <div className='grid md:grid-cols-3 justify-center items-center w-[70%] mx-auto gap-5 my-10 border-4 px-3 py-3'>
+        <div className='grid md:grid-cols-3 justify-center items-center w-[70%] mx-auto gap-5 my-10 px-3 py-3'>
             
             {
                 reviews.map(review=><ReviewCart
